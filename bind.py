8aed3e8bca9a6755e773e4192989d11905323ab6
Emmanuel Julien <e.julien.niji@gmail.com>
2017-01-02 21:33:48 +0100

    WIP

--- bind.py
+++ bind.py
@@ -4,7 +4,8 @@ from pypeg2 import re, flag, name, Plain, optional, attr, K, parse
 __header = ""
 __source = ""
 
-#--
+
+#
 typename = re.compile(r"((::)*(_|[A-z])[A-z0-9_]*)+")
 ref_re = re.compile(r"[&*]+")
 
@@ -14,13 +15,13 @@ class __CType:
 		out = ""
 		if self.const:
 			out += "const "
-		out += self.type
+		out += self.unqualified_name
 		if hasattr(self, "ref"):
 			out += self.ref
 		return out
 
 
-__CType.grammar = flag("const", K("const")), optional([flag("signed", K("signed")), flag("unsigned", K("unsigned"))]), attr("type", typename), optional(attr("ref", ref_re))
+__CType.grammar = flag("const", K("const")), optional([flag("signed", K("signed")), flag("unsigned", K("unsigned"))]), attr("unqualified_name", typename), optional(attr("ref", ref_re))
 
 
 class __CArg:
@@ -34,7 +35,7 @@ class __CArg:
 __CArg.grammar = attr("type", __CType), optional(name())
 
 
-#--
+#
 def insert_comment(comment, in_source=True, in_header=True):
 	global __header, __source
 	if in_header:
@@ -43,7 +44,17 @@ def insert_comment(comment, in_source=True, in_header=True):
 		__source += '// ' + comment + '\n'
 
 
-#--
+def insert_header_code(code):
+	global __header
+	__header += code + '\n'
+
+
+def insert_source_code(code):
+	global __source
+	__source += code + '\n'
+
+
+#
 __namespace = None
 
 
@@ -52,17 +63,33 @@ def set_namespace(ns):
 	__namespace = ns
 
 
-#--
+#
 __templates = None
 
 
-def set_templates(template):
+def set_type_templates(check, to_c, from_c):
 	global __templates
-	__templates = template
+	__templates['check'] = check
+	__templates['to_c'] = to_c
+	__templates['from_c'] = from_c
 
 
-#--
-__type_decls = {}
+def set_call_templates(arg_to_c, rval_from_c, void_rval_from_c):
+	global __templates
+	__templates['arg_to_c'] = arg_to_c
+	__templates['rval_from_c'] = rval_from_c
+	__templates['void_rval_from_c'] = void_rval_from_c
+
+
+def set_class_templates(check, to_c, from_c):
+	global __templates
+	__templates['class_check'] = check
+	__templates['class_to_c'] = to_c
+	__templates['class_from_c'] = from_c
+
+
+#
+__type_infos = None
 
 
 def get_type_clean_name(type):
@@ -79,135 +106,142 @@ def get_type_clean_name(type):
 	return '_'.join(parts)
 
 
-def get_type_typemap_op_function_name(type, op):
-	""" Return the name of the function implementing a typemap operator for a given type in the generator output."""
+def __get_type_op_function_name(type, op):
+	""" Return the name of the function implementing an operator for a given type in the generator output."""
 	return "_" + get_type_clean_name(type) + "_" + op
 
 
-def __output_type_typemap_op_function(decl, type, op):
-	name = get_type_typemap_op_function_name(type, op)
-	decl['__' + op + '_func'] = name
+def __output_type_op_function(body, type, info, op):
+	name = __get_type_op_function_name(type, op)
 
-	proto = __templates[op]
-	proto = proto.replace('$name', name)
-
-	if 'intermediate' in decl:
-		intermediate_decl = decl['intermediate']
-		proto = proto.replace('$type', intermediate_decl['type'])
-	else:
-		proto = proto.replace('$type', type)
+	proto = __templates[op](name, type, info)
 
 	global __header, __source
-
 	__header += proto + ';\n'
+	__source += proto + ' { ' + body + ' }\n'
 
-	__source += proto + ' { ' + decl[op] + ' }\n'
+	return name
 
 
-def __output_type_intermediate_functions(decl, type):
+def bind_common(type, info):
 	global __header, __source
 
-	to_type_name = "_" + get_type_clean_name(type) + "_from_" + get_type_clean_name(decl['type'])
-	from_type_name = "_" + get_type_clean_name(type) + "_to_" + get_type_clean_name(decl['type'])
-	decl['__to_type_func'] = to_type_name
-	decl['__from_type_func'] = from_type_name
+	insert_comment('typemap for ' + type)
 
-	body = decl['to_type']
-	body = body.replace('$in', 'in')
-	body = body.replace('$out', 'out')
-	__source += 'void %s(%s *in, %s *out) { %s }\n' % (to_type_name, decl['type'], type, body)
-
-	body = decl['from_type']
-	body = body.replace('$in', 'in')
-	body = body.replace('$out', 'out')
-	__source += 'void %s(%s *in, %s *out) { %s }\n' % (from_type_name, type, decl['type'], body)
-
-	__source += '\n'
+	info['type_cstr'] = '__%s_type_cstr' % type
+	insert_source_code('static const char *%s = "%s";\n\n' % (info['type_cstr'], type))
 
 
-def register_type(type, decl):
-	global __type_decls
+def bind_type(type, check, to_c, from_c):
+	"""Declare a simple type natively supported by the VM"""
+	global __type_infos
 
-	__type_decls[type] = decl
+	info = {'type': 'simple', 'check': check, 'to_c': to_c, 'from_c': from_c}
+	__type_infos[type] = info
 
-	# output type conversion functions
 	global __header, __source
+	bind_common(type, info)
 
-	comment = 'typemap for ' + type
-	if 'intermediate' in decl:
-		comment += ' (through intermediate ' + decl['intermediate']['type'] + ')'
-
-	insert_comment(comment)
-
-	__output_type_typemap_op_function(decl, type, 'check')
-	__output_type_typemap_op_function(decl, type, 'to_c')
-	__output_type_typemap_op_function(decl, type, 'from_c')
+	#
+	info['__check_func'] = __output_type_op_function(check, type, info, 'check')
+	info['__to_c_func'] = __output_type_op_function(to_c, type, info, 'to_c')
+	info['__from_c_func'] = __output_type_op_function(from_c, type, info, 'from_c')
 
 	__header += '\n'
 	__source += '\n'
 
-	if 'intermediate' in decl:
-		__output_type_intermediate_functions(decl['intermediate'], type)
 
 
-#--
-def get_bind_function_name(name):
-	return '_' + name
 
 
-def get_arg_name(i):
+
+#
+def bind_class(type, check, to_c, from_c):
+	"""Declare a C++ struct/class"""
+	global __type_infos
+
+	info = {'type': 'class', 'check': check, 'to_c': to_c, 'from_c': from_c}
+	__type_infos[type] = info
+
+	global __header, __source
+	bind_common(type, info)
+
+	#
+	info['__check_func'] = __output_type_op_function(check, type, info, 'class_check')
+	info['__to_c_func'] = __output_type_op_function(to_c, type, info, 'class_to_c')
+	info['__from_c_func'] = __output_type_op_function(from_c, type, info, 'class_from_c')
+
+	__header += '\n'
+	__source += '\n'
+
+
+
+
+
+
+
+#
+def __get_arg_name(i):
 	return 'arg' + str(i)
 
 
-def args_to_c(args):
+def __arg_to_c(args, i, args_count):
+	"""Convert an argument from a list of arguments, return the update argument position."""
 	global __header, __source
 
-	args_to_c_cast = []  # argument temporaries cast to the function signature
+	arg = args[i]  # argument to transform to C
 
-	args_len = len(args)
-	if args_len == 1 and args[0] == 'void':
-		return args_to_c_cast
+	name = __get_arg_name(i)
+	decl = __type_infos[arg.type.unqualified_name]
 
-	for i, t in enumerate(args):
-		type = Type(t)
-		decl = __type_decls[type.value_name()]
+	# by value
+	__source += arg.type.unqualified_name + ' ' + name + ';\n'
+	__source += __templates['arg_to_c'](args, i, args_count, '&' + name, decl['__to_c_func']) + '\n'
 
-		name = get_arg_name(i)
-
-		__source += type.value_name() + ' ' + name + ';\n'
-		__source += __templates['arg_to_c'](i, args_len, '&' + name, decl['__to_c_func']) + '\n'
-		args_to_c_cast.append(type.value_to_fully_qualified_transform_str() + name)
-
-	return args_to_c_cast
+	return i + 1
 
 
-def get_rval_name(i):
+
+def __args_to_c(args):
+	"""Prepare arguments to a C function call."""
+
+	args_count = len(args)
+	if args_count == 1 and args[0] == 'void':
+		return  # no arguments expected
+
+	i = 0
+	while i < args_count:
+		i = __arg_to_c(args, i, args_count)
+
+
+#
+def __get_rval_name(i):
 	return 'rval' + str(i)
 
 
-def rval_from_c(rvals):
+def __rval_from_c(rvals):
 	global __header, __source
 
-	rvals_len = len(rvals)
+	rval_count = len(rvals)
 
 	# void return value
-	if rvals_len == 1 and rvals[0] == 'void':
-		__source += __templates['void_rval_from_c'] + '\n'
+	if rval_count == 1 and rvals[0] == 'void':
+		__source += __templates['void_rval_from_c']() + '\n'
 		return
 
 	for i, rval in enumerate(rvals):
-		decl = __type_decls[rval]
+		decl = __type_infos[rval]
 
-		name = get_rval_name(i)
+		name = __get_rval_name(i)
 
 		# by value
-		__source += __templates['rval_from_c'](i, rvals_len, '&' + name, decl['__from_c_func']) + '\n'
+		__source += __templates['rval_from_c'](i, rval_count, '&' + name, decl['__from_c_func']) + '\n'
 
 
 
 
 
-
+#
 def __prepare_types(types, template):
 	if not type(types) is type([]):
 		types = [types]
@@ -222,37 +256,115 @@ def prepare_rval(rval):
 	return __prepare_types(rval, __CType)
 
 
+#
+def types_to_string(types):
+	return ','.join([repr(type) for type in types])
+
+
+# --
+def get_bind_function_name(name):
+	return '_' + name
+
 
 def bind_function(name, rval, args):
 	rval = prepare_rval(rval)
 	args = prepare_args(args)
 
 	global __header, __source
-	insert_comment('%s %s(%s)' % (rval, name, ', '.join(args)), True, False)
+	insert_comment('%s %s(%s)' % (types_to_string(rval), name, types_to_string(args)), True, False)
 
 	__source += "static int " + get_bind_function_name(name) + "(lua_State *L) {\n"
-	args_to_c_cast = args_to_c(args)  # convert args
+	__args_to_c(args)  # convert args
 
 	if rval != 'void':
-		__source += "%s %s = " % (rval, get_rval_name(0))
+		__source += "%s %s = " % (rval, __get_rval_name(0))
+
+
+
 
 	__source += name + '(' + ', '.join(args_to_c_cast) + ');\n'  # function call
 
 	rvals = [rval]  # implement *OUT (SWIG style) here
-	rval_from_c(rvals)
+	__rval_from_c(rvals)
 
 	__source += "}\n\n"
 
 
-#--
+
+
+
+
+
+
+
+
+
+
+#
 def reset_generator():
 	global __header, __source
 	__header = ""
 	__source = ""
 
-	global __templates, __type_decls
-	__templates = None
-	__type_decls = {}
+	__source += '''
+// native object wrapper
+enum OwnershipPolicy { NonOwning, ByValue, ByAddress };
+
+struct NativeObjectWrapper {
+	virtual ~NativeObjectWrapper() = 0
+
+	virtual void *GetObj() const = 0;
+	virtual const char *GetType() const = 0;
+};
+
+template <typename T> struct NativeObjectValueWrapper : NativeObjectWrapper {
+	NativeObjectValueWrapper(T *obj_, const char *type_) : obj(*obj_), type(type_) {}
+
+	void *GetObj() const override { return &obj; }
+	const char *GetType() const override { return type; }
+
+private:
+	T obj;
+	const char *type;
+};
+
+struct NativeObjectPtrWrapper : NativeObjectWrapper {
+	NativeObjectPtrWrapper(void *obj_, const char *type_) : obj(obj_), type(type_) {}
+
+	void *GetObj() const { return obj; }
+	const char *GetType() const { return type; }
+
+private:
+	void *obj;
+	const char *type;
+};
+
+template <typename T> struct NativeObjectUniquePtrWrapper : NativeObjectWrapper {
+	NativeObjectUniquePtrWrapper(T *obj_, const char *type_) : obj(obj_), type(type_) {}
+
+	void *GetObj() const { return obj.get(); }
+	const char *GetType() const { return type; }
+
+private:
+	std::unique_ptr<T> obj;
+	const char *type;
+};
+
+template <typename T> struct NativeObjectSharedPtrWrapper : NativeObjectWrapper {
+	NativeObjectSharedPtrWrapper(std::shared_ptr<T> *obj_, const char *type_) : obj(*obj_), type(type_) {}
+
+	void *GetObj() const { return obj.get(); }
+	const char *GetType() const { return type; }
+
+private:
+	std::shared_ptr<T> obj;
+	const char *type;
+};
+'''
+
+	global __templates, __type_infos
+	__templates = {}
+	__type_infos = {}
 
 
 def get_output():
--- lua.py
+++ lua.py
@@ -1,54 +1,81 @@
 import bind
 
 
-def arg_to_c(i, count, var, func):
-	return '%s(L, %d, %s);' % (func, i, var)
-
-
-def rval_from_c(i, count, var, func):
-	src = 'int rval_count = 0;\n' if i == 0 else ''
-	src += 'rval_count += %s(L, %s);\n' % (func, var)
-	if i == count - 1:
-		src += 'return rval_count'
-	return src
-
-
 def install(namespace = 'BindLua'):
 	bind.reset_generator()
 	bind.set_namespace(namespace)
 
-	#
-	bind.set_templates({
-		'check': "bool $name(lua_State *L, int idx)",
-		'to_c': "void $name(lua_State *L, int idx, $type *val)",
-		'from_c': "int $name(lua_State *L, $type *val)",
+	# templates for basic type exchange
+	def check(funcname, type, info):
+		return 'bool %s(lua_State *L, int idx)' % funcname
 
-		'arg_to_c': arg_to_c,
-		'rval_from_c': rval_from_c,
-		'void_rval_from_c': 'return 0;'
-	})
+	def to_c(funcname, type, info):
+		return 'void %s(lua_State *L, int idx, %s *val)' % (funcname, type)
 
-	#
-	bind.register_type('int', {
-		'check': "return lua_isnumber(L, idx) ? true : false;",
-		'to_c': "*val = lua_tonumber(L, idx);",
-		'from_c': "lua_pushinteger(L, *val); return 1;"
-	})
+	def from_c(funcname, type, info):
+		return 'int %s(lua_State *L, %s *val)' % (funcname, type)
 
-	bind.register_type('float', {
-		'check': "return lua_isnumber(L, idx) ? true : false;",
-		'to_c': "*val = float(lua_tonumber(L, idx));",
-		'from_c': "lua_pushnumber(L, *val); return 1;"
-	})
+	bind.set_type_templates(check, to_c, from_c)
 
-	bind.register_type('std::string', {
-		'check': "return lua_isstring(L, idx) ? true : false;",
-		'to_c': "*val = lua_tostring(L, idx);",
-		'from_c': "lua_pushstring(L, val->c_str()); return 1;"
-	})
+	# templates for function calls
+	def arg_to_c(i, count, var, func):
+		return '%s(L, %d, %s);' % (func, i, var)
 
-	bind.register_type('const char *', {
-		'check': "return lua_isstring(L, idx) ? true : false;",
-		'to_c': "*val = lua_tostring(L, idx);",
-		'from_c': "lua_pushstring(L, *val); return 1;"
-	})
+	def rval_from_c(i, count, var, func):
+		src = 'int rval_count = 0;\n' if i == 0 else ''
+		src += 'rval_count += %s(L, %s);\n' % (func, var)
+		if i == count - 1:
+			src += 'return rval_count'
+		return src
+
+	def void_rval_from_c():
+		return 'return 0;'
+
+	bind.set_call_templates(arg_to_c, rval_from_c, void_rval_from_c)
+
+	# templates for object type exchange
+	bind.insert_source_code('''
+// wrap a C object
+template<typename NATIVE_OBJECT_WRAPPER_T> int _wrap_obj(lua_State *L, void *obj, const char *type)
+{
+	auto p = lua_newuserdata(L, sizeof(NATIVE_OBJECT_WRAPPER_T));
+	if (!p)
+		return 0;
+
+	new (p) NATIVE_OBJECT_WRAPPER_T(obj, type);
+	return 1;
+}
+	''')
+
+
+
+
+#	bind.set_obj_templates(check_obj, obj_to_c, obj_from_c)
+
+
+
+
+	# bind basic types
+	bind.bind_type('int',
+		"return lua_isnumber(L, idx) ? true : false;",
+		"*val = lua_tonumber(L, idx);",
+		"lua_pushinteger(L, *val); return 1;"
+	)
+
+	bind.bind_type('float',
+		"return lua_isnumber(L, idx) ? true : false;",
+		"*val = float(lua_tonumber(L, idx));",
+		"lua_pushnumber(L, *val); return 1;"
+	)
+
+	bind.bind_type('std::string',
+		"return lua_isstring(L, idx) ? true : false;",
+		"*val = lua_tostring(L, idx);",
+		"lua_pushstring(L, val->c_str()); return 1;"
+	)
+
+	bind.bind_type('const char *',
+		"return lua_isstring(L, idx) ? true : false;",
+		"*val = lua_tostring(L, idx);",
+		"lua_pushstring(L, *val); return 1;"
+	)
