47100187e326aac32e3edbdaaf3384b2eb708f4c
Emmanuel Julien <e.julien.niji@gmail.com>
2017-01-13 20:21:23 +0100

    Add missing test files.

---
+++ tests/test_basic_type_exchange.py
@@ -0,0 +1,54 @@
+def bind_test(gen):
+	gen.start('my_test')
+
+	# inject test code in the wrapper
+	gen.insert_code('''\
+// basic interoperability
+int return_int() { return 8; }
+float return_float() { return 8.f; }
+const char *return_const_char_ptr() { return "const char * -> string"; }
+
+static int static_int = 9;
+
+int *return_int_by_pointer() { return &static_int; }
+int &return_int_by_reference() { return static_int; }
+
+// argument passing
+int add_int_by_value(int a, int b) { return a + b; }
+int add_int_by_pointer(int *a, int *b) { return *a + *b; }
+int add_int_by_reference(int &a, int &b) { return a + b; }
+''', True, False)
+
+	gen.add_include('string', True)
+
+	gen.bind_function('return_int', 'int', [])
+	gen.bind_function('return_float', 'float', [])
+	gen.bind_function('return_const_char_ptr', 'const char *', [])
+
+	gen.bind_function('return_int_by_pointer', 'int*', [])
+	gen.bind_function('return_int_by_reference', 'int&', [])
+
+	gen.bind_function('add_int_by_value', 'int', ['int', 'int'])
+	gen.bind_function('add_int_by_pointer', 'int', ['int*', 'int*'])
+	gen.bind_function('add_int_by_reference', 'int', ['int&', 'int&'])
+
+	gen.finalize()
+	return gen.get_output()
+
+
+test_python = '''\
+import my_test
+
+from tests_api import expect_eq
+
+expect_eq(my_test.return_int(), 8)
+expect_eq(my_test.return_float(), 8)
+expect_eq(my_test.return_const_char_ptr(), "const char * -> string")
+
+expect_eq(my_test.return_int_by_pointer(), 9)
+expect_eq(my_test.return_int_by_reference(), 9)
+
+expect_eq(my_test.add_int_by_value(3, 4), 7)
+expect_eq(my_test.add_int_by_pointer(3, 4), 7)
+expect_eq(my_test.add_int_by_reference(3, 4), 7)
+'''
