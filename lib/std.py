e01a9bee30daeec6c7b4be8eb1d5d2dae942557c
Emmanuel Julien <ejulien@owloh.com>
2017-07-22 21:00:49 +0200

    Finalize std::vector unit test.

--- lib/std.py
+++ lib/std.py
@@ -19,3 +19,28 @@ class SharedPtrProxyFeature:
 
 	def wrap(self, in_var, out_var):
 		return '%s = new std::shared_ptr<%s>(%s);\n' % (out_var, self.wrapped_conv.fully_qualified_name, in_var)
+
+
+class StdVectorSequenceFeature:
+	def __init__(self, wrapped_conv):
+		self.wrapped_conv = wrapped_conv
+
+	def init_type_converter(self, gen, conv):
+		pass
+
+	def get_size(self, self_var, out_var):
+		return '%s = %s->size();\n' % (out_var, self_var)
+
+	def get_item(self, self_var, idx, out_var, error_var):
+		out = 'if (%s < %s->size())\n' % (idx, self_var)
+		out += '	%s = (*%s)[%s];\n' % (out_var, self_var, idx)
+		out += 'else\n'
+		out += '	%s = true;\n' % error_var
+		return out
+
+	def set_item(self, self_var, idx, in_var, error_var):
+		out = 'if (%s < %s->size())\n' % (idx, self_var)
+		out += '	(*%s)[%s] = %s;\n' % (self_var, idx, in_var)
+		out += 'else\n'
+		out += '	%s = true;\n' % error_var
+		return out
--- tests/std_vector.py
+++ tests/std_vector.py
@@ -1,3 +1,6 @@
+from lib.std import StdVectorSequenceFeature
+
+
 def bind_test(gen):
 	gen.start('my_test')
 
@@ -7,31 +10,7 @@ def bind_test(gen):
 
 	gen.add_include('vector', is_system=True)
 
-	class StdVectorSequence:
-		def __init__(self, wrapped_conv):
-			self.wrapped_conv = wrapped_conv
-
-		def init_type_converter(self, gen, conv):
-			pass
-
-		def get_size(self, self_var, out_var):
-			return '%s = %s->size();\n' % (out_var, self_var)
-
-		def get_item(self, self_var, idx, out_var, error_var):
-			out = 'if (%s < %s->size())\n' % (idx, self_var)
-			out += '	%s = (*%s)[%s];\n' % (out_var, self_var, idx)
-			out += 'else\n'
-			out += '	%s = true;\n' % error_var
-			return out
-
-		def set_item(self, self_var, idx, in_var, error_var):
-			out = 'if (%s < %s->size())\n' % (idx, self_var)
-			out += '	(*%s)[%s] = %s;\n' % (self_var, idx, in_var)
-			out += 'else\n'
-			out += '	%s = true;\n' % error_var
-			return out
-
-	std_vector_int = gen.begin_class('std::vector<int>', features={'sequence': StdVectorSequence(gen.get_conv('int'))})
+	std_vector_int = gen.begin_class('std::vector<int>', features={'sequence': StdVectorSequenceFeature(gen.get_conv('int'))})
 	gen.bind_constructor(std_vector_int, [])
 
 	gen.bind_method(std_vector_int, 'size', 'int', [])
@@ -49,7 +28,33 @@ import my_test
 
 from tests_api import expect_eq
 
-#
-#expect_eq(n.v, 4)
+v = my_test.vector_of_int()
 
+expect_eq(v.size(), 0)
+expect_eq(len(v), 0)
+
+v.push_back(5)
+v.push_back(1)
+v.push_back(9)
+
+expect_eq(v.size(), 3)
+expect_eq(len(v), 3)
+
+expect_eq(v.at(1), 1)
+expect_eq(v.at(2), 9)
+expect_eq(v.at(0), 5)
+
+expect_eq(v[1], 1)
+expect_eq(v[2], 9)
+expect_eq(v[0], 5)
+
+v[1] = 16
+
+expect_eq(v[2], 9)
+expect_eq(v[0], 5)
+expect_eq(v[1], 16)
+
+v[0] *= 4
+
+expect_eq(v[0], 20)
 '''
