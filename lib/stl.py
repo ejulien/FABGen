a11dddcd1d7fa797244013aa9f23e5967f97526e
Emmanuel Julien <e.julien.niji@gmail.com>
2017-10-16 09:57:50 +0900

    The shared ptr proxy now uses the inline feature to prevent useless heap allocations.

--- lib/stl.py
+++ lib/stl.py
@@ -5,6 +5,7 @@ class SharedPtrProxyFeature:
 	def init_type_converter(self, gen, conv):
 		# declare shared_ptr<T> to T cast support
 		gen.add_cast(conv, self.wrapped_conv, lambda in_var, out_var: '%s = ((%s *)%s)->get();\n' % (out_var, conv.ctype, in_var))
+		conv._inline = True  # use inline alloc where possible
 
 	def unwrap(self, in_var, out_var):
 		return '%s = %s->get();\n' % (out_var, in_var)
