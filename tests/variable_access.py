043be695b6d22ee1edaa09b47191022f70adfc34
Emmanuel Julien <e.julien.niji@gmail.com>
2017-12-08 16:54:57 +0100

    Add the global variable binding unit test.

---
+++ tests/variable_access.py
@@ -0,0 +1,52 @@
+import lib
+
+
+def bind_test(gen):
+	gen.start('my_test')
+
+	lib.bind_defaults(gen)
+
+	# inject test code in the wrapper
+	gen.insert_code('''\
+int v{2};
+
+struct simple_struct {
+	int v{4};
+};
+
+simple_struct s;
+''', True, False)
+
+	simple_struct = gen.begin_class('simple_struct')
+	gen.bind_member(simple_struct, 'int v')
+	gen.end_class(simple_struct)
+
+	gen.bind_variables(['int v', 'simple_struct s'])
+
+	gen.finalize()
+	return gen.get_output()
+
+
+test_python = '''\
+import my_test
+
+assert my_test.v == 2
+my_test.v = 5
+assert my_test.v == 5
+
+assert my_test.s.v == 4
+my_test.s.v = 9
+assert my_test.s.v == 9
+'''
+
+test_lua = '''\
+my_test = require "my_test"
+
+assert(my_test.v == 2)
+my_test.v = 5
+assert(my_test.v == 5)
+
+assert(my_test.s.v == 4)
+my_test.s.v = 9
+assert(my_test.s.v == 9)
+'''
