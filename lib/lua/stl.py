4a8e828992c6436880e7513f093f5e5080949df3
Emmanuel Julien <e.julien.niji@gmail.com>
2018-02-28 14:38:07 +0100

    Fix Lua binding compilation warning.

--- lib/lua/stl.py
+++ lib/lua/stl.py
@@ -23,26 +23,26 @@ class LuaTableToStdVectorConverter(lang.lua.LuaTypeConverterCommon):
 
 	def get_type_glue(self, gen, module_name):
 		out = '''bool %s(lua_State *L, int idx) {
-	if(!lua_istable(L, idx)) {
+	if (!lua_istable(L, idx))
 		return false;
-	}
+
 	lua_pushnil(L);
-	while(lua_next(L,idx)) {
-		if(!%s(L,-1)) {
+	while (lua_next(L, idx)) {
+		if (!%s(L, -1))
 			return false;
-		}
-		lua_pop(L,1);
+		lua_pop(L, 1);
 	}
+
 	return true;
 }\n''' % (self.check_func, self.T_conv.check_func)
 
 		out += '''void %s(lua_State *L, int idx, void *obj) {
 	std::vector<%s> *sv = (std::vector<%s> *)obj;
 
-	int size = (int)lua_rawlen(L, idx);
+	size_t size = lua_rawlen(L, idx);
 	sv->resize(size);
-	for (int i = 0; i < size; ++i) {
-		lua_rawgeti(L, idx, i+1);  
+	for (size_t i = 0; i < size; ++i) {
+		lua_rawgeti(L, idx, lua_Integer(i+1));
 		%s v;
 		%s(L, -1, &v);
 		(*sv)[i] = %s;
@@ -54,9 +54,9 @@ class LuaTableToStdVectorConverter(lang.lua.LuaTypeConverterCommon):
 	
 	size_t size = sv->size();
 	lua_newtable(L);
-	for (int i = 0; i < size; ++i) {
+	for (size_t i = 0; i < size; ++i) {
 		%s(L, &sv->at(i), Copy);
-		lua_rawseti(L,-2,i+1);
+		lua_rawseti(L, -2, lua_Integer(i+1));
 	}
 	return 1;
 }\n''' % (self.from_c_func, self.T_conv.ctype, self.T_conv.ctype, self.T_conv.from_c_func)
