61099938e82d00ad62124d85b608e7acbb27c20d
Emmanuel Julien <ejulien@owloh.com>
2018-04-21 13:15:27 +0200

    Fix dir.

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -1,7 +1,9 @@
 curl http://www.lua.org/ftp/lua-5.3.2.tar.gz | tar xz
-cd lua-5.3.2
+pushd lua-5.3.2
 
 LUA_HOME_DIR=$TRAVIS_BUILD_DIR/install/lua
 
 make linux
 make INSTALL_TOP="$LUA_HOME_DIR" install
+
+popd
