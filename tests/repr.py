e2f1018d04cae7a38fd4e114fc040c09ad095f51
Emmanuel Julien <ejulien@owloh.com>
2018-04-21 12:16:28 +0200

    Add missing unit test...

---
+++ tests/repr.py
@@ -0,0 +1,36 @@
+import lib
+
+
+def bind_test(gen):
+	gen.start('my_test')
+
+	lib.bind_defaults(gen)
+
+	# inject test code in the wrapper
+	gen.insert_code('''\
+struct SomeStruct {};
+\n''', True, False)
+
+	def get_repr(var_self, var_repr):
+		return '%s = "repr!";\n' % var_repr
+
+	some_struct = gen.begin_class('SomeStruct', features={'repr': get_repr})
+	gen.bind_constructor(some_struct, [])
+	gen.end_class(some_struct)
+
+	gen.finalize()
+	return gen.get_output()
+
+
+test_python = '''\
+import my_test
+
+o = my_test.SomeStruct()
+
+assert repr(o) == "repr!"
+'''
+
+test_lua = '''\
+my_test = require "my_test"
+
+'''
